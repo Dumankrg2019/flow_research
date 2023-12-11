5acfa163c96d8e34c44d1c8fc67967d551937a0c
Dumankrg2019 <52251067+Dumankrg2019@users.noreply.github.com>
2023-12-12 00:35:26 +0600

    fix: improove flow perfomance

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -46,6 +46,8 @@ dependencies {
     implementation("com.google.android.material:material:1.10.0")
     implementation("androidx.constraintlayout:constraintlayout:2.1.4")
     implementation("androidx.lifecycle:lifecycle-viewmodel-ktx:2.6.2")
+    implementation("androidx.lifecycle:lifecycle-livedata-ktx:2.6.2")
+
 
 
     testImplementation("junit:junit:4.13.2")
--- app/src/main/java/com/example/workwithflow/crypto_app/CryptoViewModel.kt
+++ app/src/main/java/com/example/workwithflow/crypto_app/CryptoViewModel.kt
@@ -1,36 +1,50 @@
 package com.example.workwithflow.crypto_app
 
-import androidx.lifecycle.LiveData
-import androidx.lifecycle.MutableLiveData
-import androidx.lifecycle.ViewModel
+
 import androidx.lifecycle.viewModelScope
 import com.example.workwithflow.crypto_app.data.CryptoRepository
 import com.example.workwithflow.crypto_app.data.State
-import kotlinx.coroutines.delay
-import kotlinx.coroutines.launch
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.asLiveData
+import androidx.lifecycle.viewModelScope
+import kotlinx.coroutines.flow.filter
+import kotlinx.coroutines.flow.launchIn
+import kotlinx.coroutines.flow.onEach
+import kotlinx.coroutines.flow.onStart
+
+import kotlinx.coroutines.flow.filter
+import kotlinx.coroutines.flow.launchIn
+import kotlinx.coroutines.flow.map
+import kotlinx.coroutines.flow.onEach
+import kotlinx.coroutines.flow.onStart
+
 
 class CryptoViewModel : ViewModel() {
 
     val repository = CryptoRepository
 
-    private val _state = MutableLiveData<State>(State.Initial)
-    val state: LiveData<State> = _state
 
-    init {
-        loadData()
-    }
+    //стало вот так
+    val state: LiveData<State> = repository.getCurrencyList()
+        .filter { it.isNotEmpty() }
+        .map { State.Content(currencyList = it) as State}
+        .onStart { emit(State.Loading) }
+        .asLiveData()
 
-    private fun loadData() {
-        viewModelScope.launch {
-            while (true) {
-                val currentState = _state.value
-                if (currentState !is State.Content || currentState.currencyList.isEmpty()) {
-                    _state.value = State.Loading
-                }
-                val currentList = repository.getCurrencyList()
-                _state.value = State.Content(currencyList = currentList)
-                delay(3000)
-            }
-        }
-    }
+    // было вот так
+//    init {
+//        loadData()
+//    }
+//
+//    private  fun loadData() {
+//        repository.getCurrencyList()
+//            .filter { it.isNotEmpty() }
+//            .map { State.Content(currencyList = it) as State}
+//            .onStart { emit(State.Loading) }
+//            .onEach { _state.value = it }
+//            .asLiveData()
+//    }
 }
